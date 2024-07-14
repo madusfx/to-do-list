c1708f4751c28ca96b332302868594d1e9e3b9ed
madusfx <mariaesfreitas@gmail.com>
2024-07-14 18:11:29 -0300

    feat: add first section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,22 @@
 import { Header } from '@/components';
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <section className="relative flex sm:bg-background sm:bg-contain sm:bg-no-repeat sm:bg-right">
+        <div className="px-11 mt-16">
+          <h1 className="text-5xl lg:text-7xl text-black font-bold">
+            Organize
+            <br />
+            <span className="text-customGreen">your daily jobs</span>
+          </h1>
+          <p className="mt-12 font-semibold">The only way to get things done</p>
+          <button className="mt-8 px-6 py-3 rounded-lg bg-customGreen hover:scale-105 hover:transition hover:duration-700 hover:ease-in-out">
+            <p className="text-white font-semibold">Go to To-do list</p>
+          </button>
+        </div>
+      </section>
+    </>
+  );
 }
--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -3,9 +3,14 @@ import logo from '@images/coopers-logo.png';
 
 export function Header() {
   return (
-    <header className="flex flex-col p-12 sm:flex-row sm:justify-between">
-      <Image src={logo} alt="Logo Coopers Digital Production" />
-      <button className="mt-4 bg-black px-6 py-0 sm:mt-0">
+    <header className="flex flex-col p-12 items-center sm:flex-row sm:justify-between">
+      <Image
+        src={logo}
+        alt="Logo Coopers Digital Production"
+        width={217}
+        height={50}
+      />
+      <button className="mt-4 bg-black px-6 py-0 w-full sm:w-auto sm:mt-0">
         <p className="text-white">entrar</p>
       </button>
     </header>
