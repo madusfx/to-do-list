931ac67db10ef495a2737157f84f2614b40c9eef
madusfx <mariaesfreitas@gmail.com>
2024-07-15 19:10:56 -0300

    fix: fix banner background component

--- web/src/components/BannerBackground/index.tsx
+++ web/src/components/BannerBackground/index.tsx
@@ -1,13 +1,14 @@
-import './style.css';
-
 type BannerBackgroundProps = {
   children: JSX.Element | JSX.Element[];
 };
 
 export function BannerBackground({ children }: BannerBackgroundProps) {
   return (
-    <section className="relative px-20 sm:py-6 mt-36 w-full flex flex-col items-center section-bg">
-      {children}
+    <section className="mt-20 relative w-full h-72">
+      <div className="absolute inset-0 -skew-y-3 transform bg-black"></div>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full">
+        {children}
+      </div>
     </section>
   );
 }
