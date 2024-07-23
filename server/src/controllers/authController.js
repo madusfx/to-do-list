343a4499dce674c7b708f4f4f926d87b9aae9f90
madusfx <mariaesfreitas@gmail.com>
2024-07-23 10:50:15 -0300

    feat: add authentication with jwt

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const User = require('../models/user');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  })
+}
 
 router.post('/users', async (req, res) => {
   const { username, email } = req.body;
@@ -14,14 +22,36 @@ router.post('/users', async (req, res) => {
       return res.status(400).send({ error: 'this email is already registered' })
     }
 
-    user.password = undefined;
     const user = new User(req.body);
     await user.save();
-    res.status(201).send(user);
+    user.password = undefined;
+    res.status(201).send({
+      user,
+      token: generateToken({ id: user.id }),
+    });
   } catch (error) {
     res.status(400).send(error);
     console.log(error);
   }
 });
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).select('+password');
+
+  if (!user) {
+    return res.status(400).send({ error: 'user not found' });
+  }
+  if (!await bcrypt.compare(password, user.password)) {
+    return res.status(400).send({ error: 'invalid password' });
+  }
+
+  user.password = undefined;
+
+  res.send({ 
+    user, 
+    token: generateToken({ id: user.id }),
+  });
+})
 
 module.exports = (app) => app.use('/auth', router);
--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
