0e0e043360b1eae98d41b3222742eb14f4908aff
madusfx <mariaesfreitas@gmail.com>
2024-07-25 14:09:03 -0300

    style: fix confirmation modal responsiveness

--- web/src/components/ConfirmationModal/index.tsx
+++ web/src/components/ConfirmationModal/index.tsx
@@ -7,7 +7,7 @@ type ConfirmationModalProps = {
 export function ConfirmationModal({ setModalOpen }: ConfirmationModalProps) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="flex flex-col w-1/4 bg-white rounded-lg p-6 shadow-custom relative">
+      <div className="flex flex-col w-9/12 sm:w-1/4 bg-white rounded-lg p-6 shadow-custom relative">
         <p>
           Your email has been successfully sent! Someone from our team will get
           in touch with you shortly.
